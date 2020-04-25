402fcdcec128f779579b8aecad87b07ff2e437d6
Alex <alex_531@mail.ru>
2020-04-25 11:31:59 +0300

    второй урок выпонен

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 "use strict"
 
-let money = +prompt('Ваш бюджет на месяц?');
+let money = 'Ваш бюджет на месяц?';
 //    time = prompt('Введите дату в формате YYYY-MM-DD');
+
+for (let i = 0; i < 1; i++) { //проверка на пустые поля ввода бюджета
+    money = +prompt('Ваш бюджет на месяц?');
+    if (money != '') {
+        console.log('budget ok');
+    } else {
+        alert('Введите корректные данные!');
+        i--;
+    }
+};
 
 let appData = {
     budget: money,
@@ -12,7 +22,10 @@ let appData = {
     savings: false
 };
 
-for (let i = 0; i < 2; i++) {
+
+// Цикл №1
+
+for (let i = 0; i < 2; i++) { // проверка на правильность ввода расходов
     let a = prompt("Введите обязательную статью расходов в этом месяце"),
         b = +prompt("Во сколько обойдется?");
 
@@ -22,31 +35,53 @@ for (let i = 0; i < 2; i++) {
         appData.expenses[a] = b;
     } else {
         alert('Введите корректные данные!'); // домашнее задание
-        i--;        
+        i--;
     };
 };
 
-//Домашнее задание! Нерабочий скрипт
 
-// let a = prompt("Введите обязательную статью расходов в этом месяце"),
-//     b = +prompt("Во сколько обойдется?"),
-//     i = 0;
+// Цикл №2
 
-// while (i < 1) {
-//     prompt("Введите обязательную статью расходов в этом месяце"); +
-//     prompt("Во сколько обойдется?");
-//     i++;
-//     if ( a === '' || b === '') { //проверка на пустые поля ввода
-//         alert('Введите корректные данные!');
-//         i--;
-//     };
-// };
+/* let i = 0; // проверка на правильность ввода расходов
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = +prompt("Во сколько обойдется?");
+    if (typeof (a) === 'string' && (typeof (a) != null) && (typeof (b) != null) &&
+        a != '' && b != '' && a.length < 50) {
+            console.log('OK');
+            appData.expenses[a] = b;
+    }
+    else {
+        alert('Введите корректные данные');
+        i--;
+    };
+    i++;
+}; */
 
-appData.moneyPerDay = appData.budget / 30;
+
+//Цикл №3
+
+/* let i = 0; // проверка на правильность ввода расходов
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = +prompt("Во сколько обойдется?");
+    if (typeof (a) === 'string' && (typeof (a) != null) && (typeof (b) != null) &&
+        a != '' && b != '' && a.length < 50) {
+        console.log('OK');
+        appData.expenses[a] = b;
+    } else {
+        alert('Введите корректные данные');
+        i--;
+    };
+    i++;
+}
+while (i < 2); */
+
+appData.moneyPerDay = appData.budget / 30; // формула бюджета на день
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
+if (appData.moneyPerDay < 100) { //вывод уровня достатка в консоль
     console.log('Низкий уровень дохода!');
 } else if (appData.moneyPerDay > 101 && appData.moneyPerDay < 999) {
     console.log('Средний уровень дохода!');
